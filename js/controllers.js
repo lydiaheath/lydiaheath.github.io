7e04f700f6f5a00f88c47bd6827fd75db4ae726b
Finn McDonnell <finneth@gmail.com>
2014-05-15 22:46:41 +0100

    Ditch the binded buttons.

--- js/controllers.js
+++ js/controllers.js
@@ -6,7 +6,7 @@ angular.module('futures.controllers', [])
   .controller('HomeCtrl', ['$scope', function($scope) {
     $scope.tabs = [
         { title:'Dynamic Title 1', content:'Dynamic content 1' },
-        { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+        { title:'Dynamic Title 2', content:'Dynamic content 2' }
     ];
   }])
   .controller('AboutCtrl', ['$scope', function($scope) {
