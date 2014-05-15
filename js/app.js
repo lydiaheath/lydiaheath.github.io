8c43124de8ec8be4b4cd7a3d6ca1da21d5c48ff2
Finn McDonnell <finneth@gmail.com>
2014-05-15 22:23:42 +0100

    Some crappy buttons!

--- js/app.js
+++ js/app.js
@@ -2,15 +2,16 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('futures', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'futures.filters',
+  'futures.services',
+  'futures.directives',
+  'futures.controllers',
+  'ui.bootstrap'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/Home', {templateUrl: 'partials/partial1.html', controller: 'HomeCtrl'});
+  $routeProvider.when('/About', {templateUrl: 'partials/partial2.html', controller: 'AboutCtrl'});
+  $routeProvider.otherwise({redirectTo: '/Home'});
 }]);
--- js/controllers.js
+++ js/controllers.js
@@ -2,10 +2,13 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-
+angular.module('futures.controllers', [])
+  .controller('HomeCtrl', ['$scope', function($scope) {
+    $scope.tabs = [
+        { title:'Dynamic Title 1', content:'Dynamic content 1' },
+        { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+    ];
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+  .controller('AboutCtrl', ['$scope', function($scope) {
 
   }]);
