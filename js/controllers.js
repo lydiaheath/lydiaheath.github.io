bc3744fecbc8acce0bba8f5d66aa7d602459c98d
Finn McDonnell <finneth@gmail.com>
2014-05-30 23:49:56 +0100

    That was a mission. Upgrading angular-flexslider, using "track by"'s and simplifying the objects to a simple list did the trick.

--- js/controllers.js
+++ js/controllers.js
@@ -3,24 +3,33 @@
 /* Controllers */
 
 angular.module('futures.controllers', [])
-  .controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
+    .controller('NavCtrl', ['$scope', '$http', function($scope, $http) {
         $http.get('img/images/projects.json').success(function(data) {
             $scope.projects = data;
         });
+    }])
+    .controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
 
-        $http.get('img/images/images.json').success(function(data) {
-            $scope.Images = data;
+        $http.get('img/images/projects/Barrier.json').success(function(data) {
+            $scope.project = data;
         });
 
-  }])
-  .controller('ProjectCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+        $scope.SampleImages = [];
+
+        $http.get('img/images/projects.json').success(function(data) {
+            for(var key in data)
+            {
+                $scope.SampleImages.push(data[key].SampleImage);
+            }
+        });
+
+    }])
+    .controller('ProjectCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
         $scope.projectKey = $routeParams.projectKey;
 
         $http.get('img/images/projects/' + $routeParams.projectKey + '.json').success(function(data) {
             $scope.project = data;
         });
-
-  }])
-  .controller('AboutCtrl', ['$scope', function($scope) {
-
-  }]);
+    }])
+    .controller('AboutCtrl', ['$scope', function($scope) {
+    }]);
