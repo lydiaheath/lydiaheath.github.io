580192e6ce5a4d1b37f077080510c9eea3cc0741
Finn McDonnell <finneth@gmail.com>
2014-06-05 23:18:10 +0100

    Trying to solve the dynamic ng-repeat problem on first load.

--- js/controllers.js
+++ js/controllers.js
@@ -4,8 +4,19 @@
 
 angular.module('futures.controllers', [])
     .controller('NavCtrl', ['$scope', '$http', function($scope, $http) {
-        $http.get('img/images/projects.json').success(function(data) {
-            $scope.projects = data;
+        //$http.get('img/images/projects.json').success(function(data) {
+        //    $scope.projects = data;
+        //});
+        $scope.projects = {};
+
+        $.getJSON("https://spreadsheets.google.com/feeds/cells/1sUHwtUvNXpVTtLZ-WNKJryUl5DvfhJcQPKmULtqJXY8/od6/public/values?alt=json", function(data) {
+            var json = data.feed.entry[0]["gs$cell"]["$t"];
+            var obj = JSON.parse(json);
+
+            for(var key in obj) {
+                $scope.projects[key] = obj[key];
+            }
+
         });
     }])
     .controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
